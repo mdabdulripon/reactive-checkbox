67f26635b666f66dd82c77b151e94085c6ef1ad1
Warrior <warrior2110@outlook.com>
2019-05-16 18:42:29 +0200

    dev

--- src/app/checkbox-form/checkbox-form.component.ts
+++ src/app/checkbox-form/checkbox-form.component.ts
@@ -1,48 +1,119 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from "@angular/forms";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormArray,
+  FormArrayName
+} from "@angular/forms";
 import { of } from "rxjs";
-import { CheckboxFormService } from './checkbox-form.service';
+import { CheckboxFormService } from "./checkbox-form.service";
 
 @Component({
-	selector: 'app-checkbox-form',
-	templateUrl: './checkbox-form.component.html',
-	styleUrls: ['./checkbox-form.component.scss']
+  selector: "app-checkbox-form",
+  templateUrl: "./checkbox-form.component.html",
+  styleUrls: ["./checkbox-form.component.scss"]
 })
 export class CheckboxFormComponent implements OnInit {
+  public myForm: FormGroup;
+  public monthLists = [];
+  public daysLists = [];
+  public dayLists = [];
+  public experienceLists = [];
+  constructor(
+    private _fb: FormBuilder,
+    private _checkboxFormService: CheckboxFormService
+  ) {
+    of(this._checkboxFormService.getMonths()).subscribe(items => {
+      this.monthLists = items;
+    });
+    of(this._checkboxFormService.getDays()).subscribe(items => {
+      this.daysLists = items;
+    });
+    of(this._checkboxFormService.getExperience()).subscribe(items => {
+      this.experienceLists = items;
+    });
+  }
 
-	public myForm: FormGroup;
-	public monthLists = [];
-	public daysLists = [];
+  ngOnInit() {
+    this.myForm = this._fb.group({
+      months: this._fb.array([]),
+      experience: this._fb.group({
+        beginners: [""],
+        intermediate: [""],
+        advance: [""],
+        master: [""]
+      })
+    });
 
-	constructor(
-		private _fb: FormBuilder,
-		private _checkboxFormService: CheckboxFormService
-	) {
-		of(this._checkboxFormService.getMonths()).subscribe(items => {
-			this.monthLists = items;
-		});
-		of(this._checkboxFormService.getDays()).subscribe(items => {
-			this.daysLists = items;
-		});
-		this.myForm = this._fb.group({
-			months: new FormArray([]),
-			experience: this._fb.group({
-				beginners: [""],
-				intermediate: [""],
-				advance: [""],
-				master: [""]
-			})
-		});
-	}
+    for (let i = 0; i < 12; i++) {
+      this.addMonthDay(this.monthLists[i].name, 30);
+      this.dayLists.push(30);
+    }
+    console.log(this.myForm.value);
+  }
 
-	ngOnInit() { }
+  addMonthDay(monthName, day) {
+    const monthArray = this.myForm.get("months") as FormArray;
+    monthArray.push(
+      new FormGroup({
+        month: new FormControl(monthName),
+        day: new FormControl(day)
+      })
+    );
+  }
 
-	changeDays(event) {
-		console.log(event.target.value);
-	}
+  changeDays(event, item) {
+    var day = event.target.value;
+    var month = item.value;
+    this.changeDay(month, day);
+    //this.myForm.value.months.push({ month: month, days: day });
+  }
 
-	toggleMonth(item) {
-		/** 
+  changeDay(monthName, day) {
+    var cnt = this.monthLists.length;
+    for (let i = 0; i < cnt; i++) {
+      var val = this.monthLists[i].value;
+      var def = monthName;
+      if (val === def) {
+        if (this.myForm.value.months[i].month === def)
+          this.myForm.value.months[i].day = day;
+        this.dayLists[i] = day;
+      }
+    }
+  }
+
+  removeMonth(monthName) {
+    var cnt = this.myForm.value.months.length;
+    for (let i = 0; i < cnt; i++) {
+      var val = this.myForm.value.months[i].month;
+      var def = monthName;
+
+      if (val === def) {
+        console.log(val, def, i);
+        const monthArray = this.myForm.get("months") as FormArray;
+        monthArray.removeAt(i);
+      }
+    }
+  }
+
+  toggleMonth(item) {
+    item.checked = !item.checked;
+    if (item.checked === true) {
+      //this.myForm.value.months.push({ month: item.value });
+      var cnt = this.monthLists.length;
+      for (let i = 0; i < cnt; i++) {
+        var val = this.monthLists[i].value;
+        var def = item.value;
+        if (val === def) {
+          this.addMonthDay(item.value, this.dayLists[i]);
+        }
+      }
+    } else {
+      this.removeMonth(item.value);
+    }
+    /** 
 		* TODO
 		* I want all the select value fro month and day push to the months form array like the following 
 		"months": [
@@ -56,10 +127,19 @@ export class CheckboxFormComponent implements OnInit {
 		* ! Also if you dis- select a month you should remove the item from the months formArray
 		*
 		*/
-	}
+  }
 
-	save() {
-		console.log(`Data:::`, this.myForm.value);
-	}
+  onRadioChange(event) {
+    var i = 0;
+    for (i = 0; i < 4; i++) {
+      var val = event.value;
+      var index = this.experienceLists[i].value;
+      if (index === val) this.myForm.value.experience[index] = "true";
+      else this.myForm.value.experience[index] = "false";
+    }
+  }
 
+  save() {
+    console.log(`Data:::`, this.myForm.value);
+  }
 }
